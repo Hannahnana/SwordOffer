37cc0d3b0095ad0e1ad65af490a3b598a8c24473
Hannahnana <867427479@qq.com>
2020-02-17 09:34:15 +0800

    两个链表的第一个公共节点

--- Chapter4/Q52.java
+++ Chapter4/Q52.java
@@ -9,8 +9,8 @@ public class Q52 {
 
 	}
 	//借助栈
-	//时间复杂度O(m)+O(n)
-	//空间复杂度O(m)+O(n)
+	//时间复杂度O(m+n)
+	//空间复杂度O(m+n)
 	public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
 		 if(pHead1==null||pHead2==null) {
 			 return null;
@@ -37,12 +37,41 @@ public class Q52 {
 		 return p1;
     }
 	//统计长度，再从相同长度处开始遍历
-	//
+	//时间复杂度O(m+n)
+	//空间复杂度O(1)
 	public ListNode FindFirstCommonNode1(ListNode pHead1, ListNode pHead2) {
-		
+		if(pHead1==null||pHead2==null) {
+			 return null;
+		 }
+		int len1 = linkLength(pHead1);
+		int len2 = linkLength(pHead2);
+		if(len1>len2) {
+			int i=len1-len2;
+			while(i>0) {
+				pHead1 = pHead1.next;
+				i--;
+			}
+		}
+		if(len1<len2) {
+			int i=len2-len1;
+			while(i>0) {
+				pHead2 = pHead2.next;
+				i--;
+			}
+		}
+		while(pHead1!=null && pHead2!=null && pHead1!=pHead2) {
+			pHead1 = pHead1.next;
+			pHead2 = pHead2.next;
+		}
+		return pHead1;
 	}
 	public int linkLength(ListNode pHead) {
-		
+		int len = 0;
+		while(pHead!=null) {
+			len++;
+			pHead=pHead.next;
+		}
+		return len;
 	}
 	
 
